cd8001479885fe140e545a426d28b3251e195f90
sbenn1104 <stebens1104@gmail.com>
2025-01-11 17:36:17 -0500

    Update vim-options.lua

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -3,29 +3,54 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
-vim.cmd("set conceallevel=2")
 vim.g.mapleader = " "
-vim.o.clipboard = "unnamedplus"
+
+vim.keymap.set({ 'n', 'v' }, '<space>', '<Nop>', { silent = true })
+
+local opts = { noremap = true, silent = true}
+
+vim.keymap.set('n', '<C-s>', ' <cmd> w <cr>')
+vim.keymap.set('n', '<C-q>', ' <cmd> q <cr>')
+
+
+vim.keymap.set('n', 'x', '"_x', opts)
+
+
+-- Buffers
+vim.keymap.set('n', '<TAB>', ':bnext<CR>', opts)
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts)
+vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts)
+
+-- Windows
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts)
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts)
+vim.keymap.set('n', '<leader>se', '<C-w >', opts)
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts)
+
+-- Window Navigation
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+
+-- Indent Mode
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+
+-- Keep last yanked when pasting
+vim.keymap.set('v', 'p', '_dp', opts)
+
+
+
 -- black python formatting
 vim.keymap.set("n", "<leader>fmp", ":silent !black %<cr>")
 
-vim.keymap.set( {"n","t"}, "<C-q>", ":q<cr>")
-vim.keymap.set("n", "<leader>wq", ":wq<cr>")
+--Run code
+vim.keymap.set("n", "<leader>r", ":!python3 -u %<cr>")
 
-vim.keymap.set("n", "<leader>ac", ":AmazonQChat<cr>")
-vim.keymap.set("n", "<leader>o", ":AmazonQAuth<cr>")
+-- split screen
+vim.keymap.set("n", "<leader>h", "<cmd> split <cr>")
+vim.keymap.set("n", "<leader>v", "<cmd> vsplit <cr>")
 
 
-function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-end
-
--- if you only want these mappings for toggle term use term://*toggleterm#* instead
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
